a137ac21dfd8f499528a80bfa5fde42d651ead34
kleendevos <kleen.devos@gmail.com>
2017-09-19 10:58:20 +0200

    Initial commit

---
+++ src/main/java/be/vdab/allergens/AllergensApplication.java
@@ -0,0 +1,12 @@
+package be.vdab.allergens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllergensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllergensApplication.class, args);
+	}
+}
